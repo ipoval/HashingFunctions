da8f7d0d92c0d93a2844215f601fe532a0a8bf6a
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-03-09 21:08:38 -0700

    implemented Hashing functions and basic Bloom filter

--- src/bloomFilter.js
+++ src/bloomFilter.js
@@ -1,14 +1,37 @@
 var BloomFilter = function(length, hashingFunctions){
   this._length = length || 100;
   this._hashingFunctions = hashingFunctions;
-  this._data = new Array(size);
+  this._data = new Array(this._length);
 }
 
 BloomFilter.prototype.insert = function(value){
+  var self = this,
+    hashedValues = [];
 
+  this._hashingFunctions.forEach(function(hashFunc) {
+    hashedValues.push(hashFunc(self._length, value));
+  });
+
+  hashedValues.forEach(function(hashedValue) {
+    self._data[hashedValue] = 1;
+  });
 }
 
 BloomFilter.prototype.contains = function(value){
+  var self = this,
+    hashedValues = [];
 
+  this._hashingFunctions.forEach(function(hashFunc) {
+    hashedValues.push(hashFunc(self._length, value));
+  });
+
+  var doesContain = true;
+
+  hashedValues.forEach(function(hashedValue) {
+    if ( self._data[hashedValue] != 1 ) {
+      doesContain = false;
+    }
+  });
+
+  return doesContain;
 }
-
--- src/hashingFunctions.js
+++ src/hashingFunctions.js
@@ -1,13 +1,25 @@
-var hashingFunction1 = function(range, value){
-
+var hashingFunction1 = function(range, value) {
+  var sumOfCharCodes = 0;
+  for ( var i = 0; i < value.length; ++i ) {
+    sumOfCharCodes += value.charCodeAt(i);
+  }
+  return (sumOfCharCodes + value.length) % range;
 }
 
-var hashingFunction2 = function(range, value){
-
+var hashingFunction2 = function(range, value) {
+  var productOfCharCodes = 1;
+  for ( var i = 0; i < value.length; ++i ) {
+    productOfCharCodes = productOfCharCodes * value.charCodeAt(i);
+  }
+  return (productOfCharCodes + value.length) % range;
 }
 
-var hashingFunction3 = function(range, value){
-
+var hashingFunction3 = function(range, value) {
+  var productOfCharCodes = 1;
+  for ( var i = 0; i < value.length; ++i ) {
+    productOfCharCodes = productOfCharCodes * value.charCodeAt(i);
+  }
+  return (Math.floor(productOfCharCodes / value.length) + value.length) % range;
 }
 
 var hashingFunctions = [hashingFunction1, hashingFunction2, hashingFunction3];
