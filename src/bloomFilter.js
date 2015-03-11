c43fde52120f7c14a937c5219183befeafe53821
Ivan Povalyukhin <ivanpoval@gmail.com>
2015-03-10 22:27:16 -0700

    implemented hash functions and Bloom filter DS

--- src/bloomFilter.js
+++ src/bloomFilter.js
@@ -1,37 +1,22 @@
-var BloomFilter = function(length, hashingFunctions){
-  this._length = length || 100;
+var BloomFilter = function(length, hashingFunctions) {
+  this._length = length || 10000;
   this._hashingFunctions = hashingFunctions;
   this._data = new Array(this._length);
 }
 
-BloomFilter.prototype.insert = function(value){
-  var self = this,
-    hashedValues = [];
-
+BloomFilter.prototype.insert = function(value) {
+  var self = this;
   this._hashingFunctions.forEach(function(hashFunc) {
-    hashedValues.push(hashFunc(10000, value));
-  });
-
-  hashedValues.forEach(function(hashedValue) {
-    self._data[hashedValue] = 1;
+    var hash = hashFunc(self._length, value);
+    self._data[hash] = 1;
   });
 }
 
-BloomFilter.prototype.contains = function(value){
-  var self = this,
-    hashedValues = [];
-
-  this._hashingFunctions.forEach(function(hashFunc) {
-    hashedValues.push(hashFunc(10000, value));
-  });
-
-  var doesContain = true;
-
-  hashedValues.forEach(function(hashedValue) {
-    if ( self._data[hashedValue] != 1 ) {
-      doesContain = false;
-    }
-  });
-
-  return doesContain;
+BloomFilter.prototype.contains = function(value) {
+  for (var i = 0; i < this._hashingFunctions.length; ++i) {
+    var hashFunc = this._hashingFunctions[i],
+      hash = hashFunc(this._length, value);
+    if ( !this._data[hash] ) { return false; }
+  }
+  return true;
 }
--- src/hashingFunctions.js
+++ src/hashingFunctions.js
@@ -1,11 +1,12 @@
 var hashingFunction1 = function(range, value) {
-  var sumOfCharCodes = 0;
-  for ( var i = 0; i < value.length; ++i ) {
-    sumOfCharCodes += value.charCodeAt(i);
-  }
+  var sumOfCharCodes = value.split('').reduce(function(prevVal, curVal) {
+    return prevVal + curVal.charCodeAt();
+  }, 0);
+
   return (sumOfCharCodes + value.length) % range;
 }
 
+// NOTE: can add random salt to the value and apply the first hashFunc
 var hashingFunction2 = function(range, value) {
   var productOfCharCodes = 1;
   for ( var i = 0; i < value.length; ++i ) {
@@ -20,6 +21,14 @@ var hashingFunction3 = function(range, value) {
     productOfCharCodes = productOfCharCodes * value.charCodeAt(i);
   }
   return (Math.floor(productOfCharCodes / value.length) + value.length) % range;
+}
+
+var hashingFunction4 = function(range, value) {
+  var total = 0;
+  for ( var i = 0; i < value.length; ++i ) {
+    total ^= value[i].charCodeAt();
+  }
+  return total % range;
 }
 
 var hashingFunctions = [hashingFunction1, hashingFunction2, hashingFunction3];
